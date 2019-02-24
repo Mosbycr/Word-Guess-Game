0eacdb6de964a0463cd782de254454b0db1105de
Mosbycr <mosbycr@gmail.com>
2019-02-24 15:43:02 -0500

    answerArray showing as dashes on screen, userGuess toLowerCase working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,27 @@
+var answerArrayDashes = document.getElementById("answerArrayDashes");
+
+
+
 document.onkeyup = function(event) {
     var userGuess = event.key;
-    console.log(userGuess);
+    // console.log(userGuess);
+   
+    var userChoice = userGuess.toLowerCase();
+    console.log(userChoice);
 }
+
+
+var jonesWords = ["archaeology", "indiana", "whip", "marion", "ark", "adventure", "snakes"];
+
+var gameWord = jonesWords[Math.floor(Math.random() * jonesWords.length)];
+console.log(gameWord);
+
+var answerArray = [];
+for (var i = 0; i < gameWord.length; i++){
+    answerArray[i] = "_";
+}
+
+var lettersRemaining = gameWord.length;
+
+answerArrayDashes.textContent = answerArray;
 
