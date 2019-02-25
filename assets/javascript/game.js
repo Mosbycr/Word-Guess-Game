478d6de8a4589869576a2d909665779d7831654c
Mosbycr <mosbycr@gmail.com>
2019-02-24 20:48:18 -0500

    functions written, but no loops yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,70 @@
-// var answerArrayDashes = document.getElementById("answerArrayDashes");
-// var wins = document.getElementById("wins");
-// var guessesRemaining = document.getElementById("guessesRemaining");
-// var lettersGuessed = document.getElementById("lettersGuessed");
+
 
 //Score
 var wins = 0;
 var guessesRemaining = 12;
 var lettersUsed = [];
-var gameWord;
+var answerArray = [];
 
 //word choices 
 var jonesWords = ["archaeology", "indiana", "whip", "marion", "ark", "adventure", "snakes"];
 
-//picks random words
-// var gameWord = jonesWords[Math.floor(Math.random() * jonesWords.length)];
-// console.log(gameWord);
+var gameWord = jonesWords[Math.floor(Math.random() * jonesWords.length)]; 
 
+// reset function will start guessesremain over and empty out arrays to be refilled
 var resetGame = function(){
     guessesRemaining = 12;
     lettersUsed = [];
     answerArray = [];
 
-    gameWord = jonesWords[Math.floor(Math.random() * jonesWords.length)];
+    gameWord = jonesWords[Math.floor(Math.random() * jonesWords.length)]; //will set new word and array dashes
 
     for(var i = 0; i < gameWord.length; i++){
         answerArray.push("_");
     }
 
-    updateDisplay();
+    updateDashes();
+    updateGuessesRemaining();
+    updateLettersGuessed();
 }
 
-var updateDisplay = function(){
-    
+//updates Display when letters are pressed 
+var updateDashes = function(){
+    document.getElementById("answerArrayDashes").innerText = "";
+  
+    for (var i = 0; i < answerArray.length; i++){
+        document.getElementById("answerArrayDashes").innerText += answerArray[i];
+    }
+   
+}
+//updates win #
+var updateWins = function(){
+    document.getElementById("wins").innerText = wins;
+}
+//updates guesses remaining as decreases
+var updateGuessesRemaining = function(){
+    document.getElementById("guessesRemaining").innerText = guessesRemaining;
+}
+//updates letters guessed by user
+var updateLettersGuessed = function(){
+    document.getElementById("lettersGuessed").innerText = lettersUsed;
 }
 
 
 //collects keys pressed
 document.onkeyup = function(event) {
+    guessesRemaining--;
     var userGuess = event.key;
-    // console.log(userGuess);
+    //console.log(userGuess);
    
     //changes any uppercase keys to lowercase so matches words
     var userChoice = userGuess.toLowerCase();
     console.log(userChoice);
 }
 
-//fills empty array with _ _ for letters in random word to be guessed
-var answerArray = [];
-for (var i = 0; i < gameWord.length; i++){
-    answerArray[i] = "_";
-}
 
-// sets a counter for when word is filled and completed
-var lettersRemaining = gameWord.length;
+
+
 
 
 
