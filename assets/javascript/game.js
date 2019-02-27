b726bd4a8b11be233d529ce6816241453deb41c5
Mosbycr <mosbycr@gmail.com>
2019-02-27 15:13:19 -0500

    variable and letter guess function complete, dashes in correct spaces, working on event.key to link with function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
 //html DOM elements links
-var $newGameButton = document.getElementById("newGameButton");
-var $currentWordDashes = document.getElementById("currentWordDashes");
-var $lettersGuessed = document.getElementById("lettersGuessed");
-var $guessesRemaining = document.getElementById("guessesRemaining");
-var $wins = document.getElementById("wins");
+var newGameButton = document.getElementById("newGameButton");
+var currentWord = document.getElementById("currentWordDashes");
+var lettersGuess = document.getElementById("lettersGuessed");
+var guessesLeft = document.getElementById("guessesRemaining");
+var userWins = document.getElementById("wins");
 
 //variables for js code
 var mummyWords = ["archaeology", "egypt", "rick", "evelyn", "imohtep", "mummy", "curse"];
 var wins = 0;
-var guessesRemaining = 12;
+var guessesLeft = 12;
 var gameRunning = false;
-var pickedMummyWord = "";
+var pickedMummyWord = []; //changed to array from ""
 var pickedMummyWordDashesArray = [];
 var lettersGuessedArray = [];
 var incorrectLettersGuessed = [];
@@ -25,61 +25,54 @@ var resetGame = function(){
 
     pickedMummyWord = mummyWords[Math.floor(Math.random() * mummyWords.length)]; 
 
+    //puts dashes in place of letters and pushes random letter to array
     for (var i = 0; i < pickedMummyWord.length; i++) {
-        if (pickedMummyWord[i] === " ") {
-            pickedMummyWordDashesArray.push("");
-        } else {
-            pickedMummyWordDashesArray.push("_");
+            pickedMummyWordDashesArray.push("_ ");//changed to one line of code for dash and space
+    }
+
+    guessesLeft.textContent = guessesRemaining;
+    currentWord.textContent = pickedMummyWordDashesArray.join("");
+    lettersGuess.textContent = incorrectLettersGuessed;
+}
+
+newGameButton.addEventListener("click", resetGame);
+
+//letter guess function
+var userLetterPressed = function(letter){
+    console.log(letter);
+
+    if (gameRunning === true && lettersGuessedArray.indexOf(letter) === -1){//-1 means not in available letters and have not guessed yet
+        lettersGuessedArray.push(letter);
+
+        //check to see if letter is in picked word
+        for (var i=0; i < pickedMummyWord.length; i++){
+            if(pickedMummyWord[i] === letter) {
+                pickedMummyWordDashesArray[i] = pickedMummyWord[i];     
+            }
         }
+
+        currentWord.textContent = pickedMummyWordDashesArray.join("");
+    } 
+        else {
+            if (gameRunning === false){
+             alert("The game isn't running, press the New Game Button");
+            } else {
+             alert("You have already guessed that letter, please try another one!");
+            }
     }
-
-    $guessesRemaining.textContent = guessesRemaining;
-    $currentWordDashes.textContent = pickedMummyWordDashesArray;
-    $lettersGuessed.textContent = incorrectLettersGuessed;
 }
-
-
-
-
-
-// reset function will start guessesremain over and empty out arrays to be refilled
-
-
-    
-
-//updates Display when letters are pressed 
-var updateDashes = function(){
-    document.getElementById("answerArrayDashes").innerText = "";
-  
-    for (var i = 0; i < answerArray.length; i++){
-        document.getElementById("answerArrayDashes").innerText += answerArray[i];
-    }
-   
-}
-//updates win #
-// var updateWins = function(){
-//     document.getElementById("wins").innerText = wins;
-// }
-//updates guesses remaining as decreases
-// var updateGuessesRemaining = function(){
-//     document.getElementById("guessesRemaining").innerText = guessesRemaining;
-// }
-//updates letters guessed by user
-// var updateLettersGuessed = function(){
-//     document.getElementById("lettersGuessed").innerText = lettersUsed;
-// }
-
-
 //collects keys pressed
 document.onkeyup = function(event) {
-    guessesRemaining--;
+    // guessesRemaining--;
     var userGuess = event.key;
-    //console.log(userGuess);
+    console.log(userGuess);
    
     //changes any uppercase keys to lowercase so matches words
-    var userChoice = userGuess.toLowerCase();
-    console.log(userChoice);
+    userLetterPressed = userGuess.toLowerCase();
+    console.log(userLetterPressed);
 }
+
+
 
 
 
