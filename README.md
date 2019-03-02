0c1f21e50e20fef4998f3ba0d41b87f79af9c3cc
Mosbycr <mosbycr@gmail.com>
2019-03-01 22:45:19 -0500

    readme complete, adding audio

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Word-Guess-Game
-Word Guess Game similar to Hangman utilizing HTML5, CSS3, Bootstrap4, and Javascript
+# The Egyptian Mummy Word Guess Game
+A Word Guess Game similar to Hangman utilizing HTML5, CSS3, Bootstrap4, and Javascript. 
+
+# How the game works
+An array of words provides the current hidden word. Each word is presented in random order. As letters are guessed correctly they are filled in until the user either fills in the entire word and wins or runs out of the 10 guesses available. A start game button with an .addEventListener allows users to begin the game with all variables reset to their default values. As users press keyboard letters a document.onkeyup event captures the key and runs the letter chosen through a function to compare that letter to those in the current hidden word. Letters that match those within the word are filled in while letters that do not match the hidden word are pushed to an empty array and displayed as letters already guessed. Using two seperate functions containing if statements the game checks for a loss and a win. As letters are guessed incorrectly the user decreases the amount of guesses left. If guesses left becomes 0 the game ends and all variables are reset. If the user correctly guesses the word. The wins score increases and a new word is generated. The game continues on. 
+
+# Game Rules
+1. Start the game by pressing the Press here to start a new game button.
+2. Then using the keyboard the user can press any letter key to guess the letters in the current word.
+3. The user has 10 guesses to complete the hidden word before losing and a new game begins.
+4. If the user completes the hidden word, they win and the score increases to 1. A new word is generated and the game continues.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,21 @@
-//html DOM elements links
 var newGameButton = document.getElementById("newGameButton");
 var currentWord = document.getElementById("currentWordDashes");
 var lettersGuess = document.getElementById("lettersGuessed");
 var guessNumber = document.getElementById("guessesRemaining");
 var userWins = document.getElementById("wins");
 
-//variables for js code
 var mummyWords = ["Archaeology", "Egypt", "Tomb", "Sarcophagus", "Afterlife", "Mummy", "Curse"];
 var wins = 0;
 var guessesLeft = 10;
 var gameRunning = false;
-var pickedMummyWord = []; //changed to array from ""
+var pickedMummyWord = []; 
 var pickedMummyWordDashesArray = [];
 var lettersGuessedArray = [];
 var incorrectLettersGuessed = [];
 
-//reset function
 var resetGame = function(){
     gameRunning = true;
+    wins = 0;
     guessesLeft = 10;
     lettersGuessedArray = [];
     incorrectLettersGuessed = [];
@@ -25,27 +23,45 @@ var resetGame = function(){
 
     pickedMummyWord = mummyWords[Math.floor(Math.random() * mummyWords.length)]; 
 
-    //puts dashes in place of letters and pushes random letter to array
     for (var i = 0; i < pickedMummyWord.length; i++) {
-            pickedMummyWordDashesArray.push("_ ");//changed to one line of code for dash and space
+            pickedMummyWordDashesArray.push("_ ");
     }
 
     guessNumber.textContent = guessesLeft;
     currentWord.textContent = pickedMummyWordDashesArray.join("");
     lettersGuess.textContent = incorrectLettersGuessed;
+    userWins.textContent = wins;
 }
 
-//start new game
+var resetGameWin = function () {
+    gameRunning = true;
+    guessesLeft = 10;
+    lettersGuessedArray = [];
+    incorrectLettersGuessed = [];
+    pickedMummyWordDashesArray = [];
+
+    pickedMummyWord = mummyWords[Math.floor(Math.random() * mummyWords.length)];
+
+    for (var i = 0; i < pickedMummyWord.length; i++) {
+        pickedMummyWordDashesArray.push("_ ");
+    }
+
+    guessNumber.textContent = guessesLeft;
+    currentWord.textContent = pickedMummyWordDashesArray.join("");
+    lettersGuess.textContent = incorrectLettersGuessed;
+
+    var snd = new Sound("../audio/magical.mp3");
+    snd.play();
+}
+
 newGameButton.addEventListener("click", resetGame);
 
-//letter guess function
 var userLetterPressed = function(letter){
     console.log(letter);
 
-    if (gameRunning === true && lettersGuessedArray.indexOf(letter) === -1){//-1 means not in available letters and have not guessed yet
+    if (gameRunning === true && lettersGuessedArray.indexOf(letter) === -1){
         lettersGuessedArray.push(letter);
 
-        //check to see if letter is in picked word
         for (var i=0; i < pickedMummyWord.length; i++){
             if(pickedMummyWord[i].toLowerCase() === letter.toLowerCase()) {
                 pickedMummyWordDashesArray[i] = pickedMummyWord[i];        
@@ -55,18 +71,10 @@ var userLetterPressed = function(letter){
         currentWord.textContent = pickedMummyWordDashesArray.join("");  
         checkLetter(letter);
     } 
-    
-        // else {
-        //     if (gameRunning === false){
-        //      alert("The game isn't running, press the New Game Button");
-        //     } else {
-        //      alert("You have already guessed that letter, please try another one!");
-        //     }
-    // }
 }
 
 var checkLetter = function(letter){
-    if (pickedMummyWordDashesArray.indexOf(letter.toLowerCase()) === -1) { // need to work more is returning 5 of the letter
+    if (pickedMummyWordDashesArray.indexOf(letter.toLowerCase()) === -1) { 
         incorrectLettersGuessed.push(letter);
         lettersGuess.textContent = incorrectLettersGuessed.join("");
         guessesLeft--;
@@ -77,7 +85,7 @@ var checkLetter = function(letter){
 
 var checkLose = function(){
     if (guessesLeft === 0){
-        gameRunning = false; // maybe add alert here to say lost???
+        gameRunning = false; 
         currentWord.textContent = pickedMummyWord;
         resetGame();
     }
@@ -89,16 +97,14 @@ var checkWin = function(){
         wins++;
         userWins.textContent = wins;
         gameRunning = false;
-        resetGame();
+        resetGameWin();
     }
 }
 
-
-//collects keys pressed
-document.onkeyup = function(event) { // can add specific keys so only letters
-              // if(even.keyCode >= 65 && event.keyCode <= 90)
+document.onkeyup = function(event) { 
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
     userLetterPressed(event.key);
-    
+    }
 }
 
 
